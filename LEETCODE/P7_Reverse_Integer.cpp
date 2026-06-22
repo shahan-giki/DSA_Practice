727c102259fdfaa3cc90d66f6802ff7dd2828ec8
shahan-giki <alishahan988@gmail.com>
2026-06-23 02:28:27 +0500

    Add reverse integer solution

---
+++ LEETCODE/P7_Reverse_Integer.cpp
@@ -0,0 +1,19 @@
+//Given a signed 32-bit integer x, return x with its digits reversed. If reversing x 
+//causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then 
+//return 0.
+
+class Solution {
+public:
+    int reverse(int x) {
+    int answer=0;
+    while(x!=0){
+    int number = x%10;
+    if ((answer > INT_MAX/10) || (answer<INT_MIN/10)){
+        return 0;
+    }
+    answer=(answer*10)+number; 
+    x = x/10;
+    }
+    return answer;
+    }
+};
